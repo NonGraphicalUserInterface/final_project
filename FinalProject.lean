1594e1b3098acd17d3af1c34c23f2baeb0fd1621
Alex Wong <alwongfu@gmail.com>
2026-05-05 23:33:47 -0400

    Final project done

--- FinalProject.lean
+++ FinalProject.lean
@@ -1,1 +1,64 @@
-import FinalProject.Basic
+import Mathlib.Data.Real.Sqrt
+import Mathlib.Tactic
+
+open Real -- like namespace in C++
+
+/- GOAL: to prove Binet's Theorem for all n ∈ ℕ, n > 0 -/
+
+/- The Golden Ratio and its ``conjugate" -/
+noncomputable def φ : ℝ := (1 + √5) / 2
+noncomputable def ψ : ℝ := (1 - √5) / 2
+
+def F : ℕ → ℕ
+| 0 => 0
+| 1 => 1
+| (n+2) => F n + F (n + 1)
+
+/- Goal: prove, for conditions listed above, that F_n = (φ^n - ψ^n) / √5 -/
+-- Additional lemmas: needed because Lean wasn't simplifying the way I wanted and I also needed
+-- to introduce some new lemmas about the behaviour of φ and ψ anyway.
+lemma φ_plus_ψ : φ + ψ = 1 := by
+  rw[ψ, φ]
+  ring
+
+lemma φ_times_ψ : φ*ψ = -1 := by
+  rw[ψ, φ]
+  ring_nf -- Lean told me to do this -- I know we didn't cover it in class
+  simp
+  ring
+
+lemma k_plus_two (k : ℕ) (hn : 0 ≤ k) : φ^(k+2) - ψ^(k+2) = φ^k * (1 + φ) - ψ^k * (1 + ψ) := by
+  calc
+    φ^(k+2) - ψ^(k+2) = φ^k*((φ+ψ)^2 - φ*ψ - ψ*(φ+ψ)) - ψ^k*((φ+ψ)^2 - φ*ψ - φ*(φ+ψ)) := by ring
+    _ = φ^k*(1^2 - (-1) - ψ*1) - ψ^k*(1^2 - (-1) - φ*1) := by rw[φ_plus_ψ, φ_times_ψ]
+    _ = φ^k*(2 - ψ) - ψ^k*(2 - φ) := by ring
+    _ = φ^k*(2 - (1 - √5) / 2) - ψ^k*(2 - (1 + √5) / 2) := by rw[φ, ψ]
+    _ = φ^k*(1 + (1 + √5) / 2) - ψ^k*(1 + (1 - √5) / 2) := by ring
+
+theorem binet (n : ℕ) (hn : 0 ≤ n) : F n = (φ^n - ψ^n) / sqrt 5 := by
+  -- Initiate induction (I know that this is different from Macbeth, but just thought it
+  -- would be a good chance to get used to more standard Lean)
+  induction n using Nat.twoStepInduction with
+  | zero =>
+    simp
+  | one =>
+    simp only [pow_one] -- Lean itself told me to use "only [pow_one]"; same as "simp"
+    rw [F, φ, ψ]
+    ring_nf -- Lean told me to use _nf, even though it doesn't seem to make any difference in goal
+    -- ring
+    have h_sqrt : √5 ≠ 0 := by
+      have h_5 : (5 : ℝ) ≠ 0 := by norm_num
+      have h_leq : (0 : ℝ) ≤ 5 := by norm_num
+      rw[sqrt_ne_zero]
+      · exact h_5
+      exact h_leq
+    simp
+  | more k ih_k ih_k1 =>
+    rw[F]
+    simp only [Nat.cast_add] -- Again, Lean told me to add this and I didn't like the yellow line
+    rw[ih_k, ih_k1]
+    · rw[k_plus_two]
+      · ring
+      · linarith
+    · linarith
+    · linarith
---
+++ sumofcubes.lean
@@ -0,0 +1,99 @@
+import Mathlib.Tactic
+
+/- Main goal: prove that $∑ _{i=0}^n (i^3) = (∑ _{i=0}^n (i))^2$ -/
+/- We first prove that $∑_{i=0}^n i = n(n+1)/2$, which gives a formula that is easier to use -/
+
+def S
+| 0 => 0
+| n + 1 => S n + (n+1)
+
+def C
+| 0 => 0
+| n + 1 => C n + (n+1)^3
+
+lemma prod_succ_even (n : ℕ) : Even (n*(n+1)) := by
+  dsimp [Even]
+  ring
+
+  cases Nat.even_or_odd n with
+  | inl h_even =>
+    obtain ⟨k, hk⟩ := h_even
+    use k*(2*k+1)
+    rw [hk]
+    ring
+  | inr h_odd =>
+    obtain ⟨k, hk⟩ := h_odd
+    use (2*k+1)*(k+1)
+    rw[hk]
+    ring
+
+theorem sqsum (n : ℕ) (hn : 0 ≤ n) : S n = n*(n+1) / 2 := by
+  -- Initiate induction (I know that this is different from Macbeth, but just thought it
+  -- would be a good chance to get used to more standard Lean)
+  induction n with
+  | zero =>
+    rw[S]
+  | succ n ih =>
+    have h_gauss_times_2: 2*(S (n+1)) = (n+1)*(n+1+1) := by
+      have h_prod_even := prod_succ_even n
+      obtain ⟨r, hr⟩ := h_prod_even
+      have h_2_div_r_plus_r :  2 ∣ (r+r) := by
+        use r
+        ring
+      calc
+        2 * S (n + 1) = 2 * (S n + (n + 1)) := by rw [S]
+        _ = 2 * S n + 2 * (n + 1) := by ring
+        _ = 2 * ((r + r) / 2) + 2 * (n + 1) := by rw [ih (Nat.zero_le n), hr]
+         -- mul_div_cancel'-- allows the twos to be cancelled, but we must prove divisibility
+        _ = (r+r) + 2*(n+1) := by rw[Nat.mul_div_cancel' (h_2_div_r_plus_r)]
+        _ = n*(n+1) + 2*(n+1) := by rw[hr]
+        _ = (n+1)*(n+2) := by ring
+    have : Even (S (n+1)*2 : ℕ) := by
+      have : S (n+1) * 2 = 2 * S (n+1) := by ring
+      rw[this]
+      rw[h_gauss_times_2]
+      apply prod_succ_even
+    obtain ⟨r, hr⟩ := this
+    calc
+    -- norm_num needed since must prove 0 < 2
+      S (n+1) = (S (n+1)*2) / 2 := by rw[Nat.mul_div_cancel]; norm_num
+      _ = (2*S (n+1)) / 2 := by ring
+      _ = (n+1)*(n+2) / 2 := by rw[h_gauss_times_2]
+
+theorem cbsum (n : ℕ) (hn : 0 ≤ n) : C n = (S n)^2 := by
+  -- Induction again :)
+  induction n with
+  | zero =>
+    rw[C, S]
+    norm_num
+  | succ n ih =>
+    have h_2_div_r_plus_r: 2 ∣ n*(n+1) := by
+      obtain ⟨r, hr⟩ := prod_succ_even n
+      use r
+      rw[hr]
+      ring
+    calc
+      C (n+1) = C n + (n+1)^3 := by rw[C]
+      _ = (S n)^2 + (n+1)^3 := by rw[ih (Nat.zero_le n)]
+      _ = (n*(n+1)/2)^2 + (n+1)^3 := by rw[sqsum n (Nat.zero_le n)]
+      _ = (n*(n+1)/2)^2 + ((n+1)^3*4)/4 := by rw[Nat.mul_div_cancel]; norm_num
+      _ = (n*(n+1))^2 / 4 + ((n+1)^3*4) / 4 := by rw[Nat.div_pow (h_2_div_r_plus_r)];
+      _ = ((n*(n+1))^2 + ((n+1)^3*4)) / 4 := by
+        -- Over integers, a / c + b / c ≠ (a+b) / c in general; there is an added term (see def.)
+        -- Our goal is to prove that the added term is zero, which we do by proving that n*(n+1)
+        -- is divided evenly by 4 and that 0 < 4.
+        rw[Nat.add_div]
+        · norm_num
+          obtain ⟨r, hr⟩ := prod_succ_even n
+          rw[hr]
+          ring
+          norm_num
+        · norm_num
+      _ = ((n+1)*(n+2))^2 / 4 := by ring
+      _ = ((n+1)*(n+2) / 2)^2 := by
+        rw[Nat.div_pow]
+        · obtain ⟨r, hr⟩ := prod_succ_even (n+1)
+          use r
+          simp[hr]
+          ring
+      _ = (S (n+1))^2 := by rw[sqsum (n+1) (Nat.zero_le (n+1))]
